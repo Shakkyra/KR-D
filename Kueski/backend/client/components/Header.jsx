168e008608b90dac5b8c02b619c1875c8e462118
Shakkyra <86767990+Shakkyra@users.noreply.github.com>
2023-04-14 11:37:22 -0600

    Update

---
+++ Kueski/backend/client/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <div className='flex justify-between px-4 pt-4'>
+                <h2>Dashboard</h2>
+                <h2>Welcome Back, Kueski</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Header
+
+
+
