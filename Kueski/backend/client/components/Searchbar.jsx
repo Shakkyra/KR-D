c3e8b715b1c37d9b766d57f42e4ea55af5611bb9
Shakkyra <86767990+Shakkyra@users.noreply.github.com>
2023-04-19 08:33:37 -0600

    Update de componentes
    Searchbar y Visualizer

--- Kueski/backend/client/components/Searchbar.jsx
+++ Kueski/backend/client/components/Searchbar.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 
 const Searchbar = () => {
-    return (
-        <div className="shadow-lg rounded bg-white">
-            <div className="relative mb-3" data-te-input-wrapper-init>
-                <input
-                    type="text"
-                    className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-600 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                    id="exampleFormControlInputText"
-                    placeholder="Example label" />
-                <label
-                    htmlFor="exampleFormControlInputText"
-                    className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary"
-                >Search
-                </label>
-            </div>
-        </div>
-    )
-}
+  return (
+    <>
+      {/* Heads up! 👋 Plugins: - @tailwindcss/forms */}
+      <label
+        htmlFor="UserEmail"
+        className="relative block overflow-hidden border-b border-gray-200 pt-3 focus-within:border-blue-600"
+      >
+        <input
+          type="email"
+          id="UserEmail"
+          placeholder="Email"
+          className="peer h-8 w-full border-none bg-transparent p-0 placeholder-transparent focus:border-transparent focus:outline-none focus:ring-0 sm:text-sm"
+        />
+
+        <span
+          className="absolute start-0 top-2 -translate-y-1/2 text-xs text-gray-700 transition-all peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-sm peer-focus:top-2 peer-focus:text-xs"
+        >
+          Search
+        </span>
+      </label>
+    </>
+  );
+};
 
 export default Searchbar;
