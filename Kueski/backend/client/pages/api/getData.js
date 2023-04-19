610edadd2544d121faa9422bb0bb0fd1365531d8
Juan Zambrano <jpablozambrano@hotmail.com>
2023-04-19 17:46:19 -0600

    Successful connection to database

---
+++ Kueski/backend/client/pages/api/getData.js
@@ -0,0 +1,21 @@
+import mysql from "mysql2/promise";
+
+export default async function handler(req, res) {
+    const dbconnection = await mysql.createConnection({
+        host: "localhost",
+        database: "empresa",
+        user: "root",
+    });
+    try{
+        const query = "SELECT EMP_NUM, EMP_LNAME, EMP_FNAME FROM employee";
+        const values = [];
+        const [data] = await dbconnection.execute(query, values);
+        dbconnection.end();
+
+        res.status(200).json({ name: data })
+    }catch (error) {
+        res.status(500).json({error: error.message});
+    }
+    
+  }
+  
