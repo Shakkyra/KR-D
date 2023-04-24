41eb6b68b2a133fe7260fa27879f57bd9f281bbe
Shakkyra <86767990+Shakkyra@users.noreply.github.com>
2023-04-24 10:25:29 -0600

    Updates Etiquetas
    Correcion de nombres de etiquetas

--- Kueski/backend/client/components/Acceso.jsx
+++ Kueski/backend/client/components/Acceso.jsx
@@ -2,88 +2,88 @@ import React from "react";
 
 const Acceso = () => {
     return(
-        <div class="flex flex-col space-y-0">
-            <div class="px-4 sm:px-0">
-                <h3 class="text-base font-semibold leading-7 text-gray-900">Información</h3>
+        <div className="flex flex-col space-y-0">
+            <div className="px-4 sm:px-0">
+                <h3 className="text-base font-semibold leading-7 text-gray-900">Información</h3>
             </div>
-            <div class="mt-6 border-t border-blue-100">
-                <dl class="divide-y divide-blue-100">
+            <div className="mt-6 border-t border-blue-100">
+                <dl className="divide-y divide-blue-100">
                     
-                    <div class="sm:grid sm:grid-cols-3">
+                    <div className="sm:grid sm:grid-cols-3">
                         
-                        <div class="px-4 py-3">
-                            <dt class="text-sm font-medium leading-6 text-gray-900">Nombre</dt>
-                            <dd class="mt-1 text-sm leading-6 text-gray-700">Margot</dd>
+                        <div className="px-4 py-3">
+                            <dt className="text-sm font-medium leading-6 text-gray-900">Nombre</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-700">Margot</dd>
                         </div>
 
-                        <div class="px-4 py-3 sm:col-span-2">
-                            <dt class="text-sm font-medium leading-6 text-gray-900">País</dt>
-                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
+                        <div className="px-4 py-3 sm:col-span-2">
+                            <dt className="text-sm font-medium leading-6 text-gray-900">País</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
                         </div>
 
-                        <div class="px-4 py-3">
-                            <dt class="text-sm font-medium leading-6 text-gray-900">Primer Apellido</dt>
-                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
+                        <div className="px-4 py-3">
+                            <dt className="text-sm font-medium leading-6 text-gray-900">Primer Apellido</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
                         </div>
 
-                        <div class="px-4 py-3 sm:col-span-2">
-                            <dt class="text-sm font-medium leading-6 text-gray-900">Estado</dt>
-                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
+                        <div className="px-4 py-3 sm:col-span-2">
+                            <dt className="text-sm font-medium leading-6 text-gray-900">Estado</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
                         </div>
 
-                        <div class="px-4 py-3">
-                            <dt class="text-sm font-medium leading-6 text-gray-900">Segundo Apellido</dt>
-                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
+                        <div className="px-4 py-3">
+                            <dt className="text-sm font-medium leading-6 text-gray-900">Segundo Apellido</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
                         </div>
 
-                        <div class="px-4 py-3 sm:col-span-2">
-                            <dt class="text-sm font-medium leading-6 text-gray-900">Ciudad</dt>
-                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
+                        <div className="px-4 py-3 sm:col-span-2">
+                            <dt className="text-sm font-medium leading-6 text-gray-900">Ciudad</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
                         </div>
 
-                        <div class="px-4 py-3">
-                            <dt class="text-sm font-medium leading-6 text-gray-900">Fecha de Nacimiento</dt>
-                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
+                        <div className="px-4 py-3">
+                            <dt className="text-sm font-medium leading-6 text-gray-900">Fecha de Nacimiento</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
                         </div>
 
-                        <div class="px-4 py-3 sm:col-span-2">
-                            <dt class="text-sm font-medium leading-6 text-gray-900">C.P.</dt>
-                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
+                        <div className="px-4 py-3 sm:col-span-2">
+                            <dt className="text-sm font-medium leading-6 text-gray-900">C.P.</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
                         </div>
 
-                        <div class="px-4 py-3">
-                            <dt class="text-sm font-medium leading-6 text-gray-900">Nacionalidad</dt>
-                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
+                        <div className="px-4 py-3">
+                            <dt className="text-sm font-medium leading-6 text-gray-900">Nacionalidad</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
                         </div>                        
 
-                        <div class="px-4 py-3 sm:col-span-2">
-                            <dt class="text-sm font-medium leading-6 text-gray-900">Num. Exterior</dt>
-                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
+                        <div className="px-4 py-3 sm:col-span-2">
+                            <dt className="text-sm font-medium leading-6 text-gray-900">Num. Exterior</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
                         </div>
 
-                        <div class="px-4 py-3">
-                            <dt class="text-sm font-medium leading-6 text-gray-900">Empleo</dt>
-                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
+                        <div className="px-4 py-3">
+                            <dt className="text-sm font-medium leading-6 text-gray-900">Empleo</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
                         </div>
 
-                        <div class="px-4 py-3 sm:col-span-2">
-                            <dt class="text-sm font-medium leading-6 text-gray-900">Num. Interior</dt>
-                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-1 sm:mt-0">Margot</dd>
+                        <div className="px-4 py-3 sm:col-span-2">
+                            <dt className="text-sm font-medium leading-6 text-gray-900">Num. Interior</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-1 sm:mt-0">Margot</dd>
                         </div>
 
-                        <div class="px-4 py-3">
-                            <dt class="text-sm font-medium leading-6 text-gray-900">CURP</dt>
-                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
+                        <div className="px-4 py-3">
+                            <dt className="text-sm font-medium leading-6 text-gray-900">CURP</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
                         </div>
 
-                        <div class="px-4 py-3 sm:col-span-2">
-                            <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"> Compartir </button>
-                            <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"> Regresar </button>
+                        <div className="px-4 py-3 sm:col-span-2">
+                            <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"> Regresar </button>
                         </div>
 
-                        <div class="px-4 py-3">
-                            <dt class="text-sm font-medium leading-6 text-gray-900">Sexo</dt>
-                            <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
+                        <div className
+                        ="px-4 py-3">
+                            <dt className="text-sm font-medium leading-6 text-gray-900">Sexo</dt>
+                            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">Margot</dd>
                         </div>
 
                     </div>
