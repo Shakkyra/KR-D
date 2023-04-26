ed7068dc8e0cec5900b875b7e4de31d352a7d662
StefanoHA <stefano.herrejon@hotmail.com>
2023-04-25 21:51:28 -0600

    Home detalles

--- Kueski/backend/client/components/Welcome.jsx
+++ Kueski/backend/client/components/Welcome.jsx
@@ -8,15 +8,14 @@ const Welcome = () => {
         >
             <div className="mx-auto max-w-xl text-center">
             <h1 className="text-3xl font-extrabold sm:text-5xl">
-                Understand User Flow.
+                Kueski
                 <strong className="font-extrabold text-red-700 sm:block">
-                Increase Conversion.
+                Derechos ARCO.
                 </strong>
             </h1>
 
             <p className="mt-4 sm:text-xl/relaxed">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo
-                tenetur fuga ducimus numquam ea!
+                Interfaz para poder aplicar y respetar los derechos sobre la información.
             </p>
 
             <div className="mt-8 flex flex-wrap justify-center gap-4">
@@ -24,7 +23,7 @@ const Welcome = () => {
                 className="block w-full rounded bg-red-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-red-700 focus:outline-none focus:ring active:bg-red-500 sm:w-auto"
                 href="/dashboard"
                 >
-                Get Started
+                Dashboard
                 </a>
 
                 <a
