52c583abac4ea5f04f6cfe97b92eb24d2f30b3dc
Shakkyra <86767990+Shakkyra@users.noreply.github.com>
2023-04-25 21:41:02 -0600

    Welcome page

--- Kueski/backend/client/pages/index.js
+++ Kueski/backend/client/pages/index.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import Header from '../components/Header';
 import Searchbar from '../components/Searchbar';
 import kueski_fondo from '../public/imagenes/kueski_fondo.png';
-import Welcome from '../components/Welcome';
+import Welcome from '@/components/Welcome';
 
 {/* Pagina de INICIO */}
 
@@ -17,19 +17,19 @@ const Home = () => {
         <link rel='icon' href='/kueskiLogo.png' />
       </Head>
         <Header />
-        <main className='bg-gray-100 min-h-screen'></main>
         <h1 className='p-4'>HOME</h1>
+        <Welcome/>
+        <main className='bg-gray-100 min-h-screen' style={{backgroundImage: `url(${kueski_fondo})`}}>
 
         <div className='p-4'>
-          <Welcome/>
           {/* Agregamos las clases flex flex-col items-center para crear un contenedor flexible vertical y centrado */}
           <div className='flex flex-col'>
 
           </div>
         </div>
-      
+      </main>
     </>
-  )
+  );
 };
 
 export default Home;
