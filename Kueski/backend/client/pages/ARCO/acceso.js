b0d6c50f0e95e56ba5fb36022e79df7afdedd33f
Juan Zambrano <jpablozambrano@hotmail.com>
2023-05-03 15:18:15 -0600

    Update acceso.js

--- Kueski/backend/client/pages/ARCO/acceso.js
+++ Kueski/backend/client/pages/ARCO/acceso.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
 import Header from '../../components/Header';
-import kueski_fondo from '@/public/imagenes/kueski_fondo.png';
 import { useRouter } from 'next/router';
 
 {/* Pagina de Acceso */}
@@ -20,14 +19,12 @@ const Acceso = () => {
       </Head>
       <Header />
       <h1 className='p-4'>Acceso</h1>
-      <main className='bg-gray-100 min-h-screen' style={{backgroundImage: `url(${kueski_fondo})`}}>
-        <div className='p-4'>
+      <div className='p-4'>
           {/* Agregamos las clases flex flex-col items-center para crear un contenedor flexible vertical y centrado */}
           <div className='flex flex-col'>
             <h2>Bienvenido, {userId}!</h2>
           </div>
         </div>
-      </main>
     </>
   );
 };
