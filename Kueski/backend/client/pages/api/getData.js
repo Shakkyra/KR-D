e5a95a4b97620857da512070f41431fe6de3a6a5
Juan Zambrano <jpablozambrano@hotmail.com>
2023-05-03 20:15:46 -0600

    Added Full Name (de nuevo)

--- Kueski/backend/client/pages/api/getData.js
+++ Kueski/backend/client/pages/api/getData.js
@@ -7,7 +7,7 @@ export default async function handler(req, res) {
         const searchValue = req.query.searchValue || "";
         //console.log("Valor del parámetro de búsqueda en getData2.js:", searchValue);
         // Modificar la consulta para filtrar por el valor del parámetro de búsqueda
-        const query = "SELECT USER_ID, FIRST_LAST_NAME, SECOND_LAST_NAME, PHONE_NUMER, EMAIL FROM USERS WHERE USER_ID LIKE ?";
+        const query = "SELECT USER_ID, USER_NAME, FIRST_LAST_NAME, SECOND_LAST_NAME, PHONE_NUMER, EMAIL FROM USERS WHERE USER_ID LIKE ?";
         const values = [`%${searchValue}%`];
         const [data] = await dbconnection.execute(query, values);
         dbconnection.end();
