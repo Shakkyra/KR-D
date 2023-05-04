2a20739f447e9336fbd1d2f059b9d626ce9792ae
Juan Zambrano <jpablozambrano@hotmail.com>
2023-05-04 17:16:49 -0600

    Rectificacion working

--- Kueski/backend/client/pages/api/putArcoOposicion.js
+++ Kueski/backend/client/pages/api/putArcoOposicion.js
@@ -3,13 +3,10 @@ import dbConnection from "@/db";
 export default async function handler(req, res) {
     try {
       const dbconnection = await dbConnection();
-      // Obtener el valor del parámetro de búsqueda
-      const searchValue = req.query.searchValue || "";
-      // Modificar la consulta para filtrar por el valor del parámetro de búsqueda
+      
       const query = "CALL sql9615348.OPP(?,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1);";
-      const [data] = await dbconnection.execute(query, [searchValue]);
+      dbconnection.execute(query, [searchValue]);
       dbconnection.end();
-      res.status(200).json({ names: data });
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
