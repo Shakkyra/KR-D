f4c8e65ed929c8929cf61964ca7fe7cfaf662b32
Shakkyra <86767990+Shakkyra@users.noreply.github.com>
2023-05-04 23:11:33 -0600

    Update historial.js

--- Kueski/backend/client/pages/ARCO/historial.js
+++ Kueski/backend/client/pages/ARCO/historial.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 import Header from '../../components/Header';
 import { useRouter } from 'next/router';
-import Historialcom from '@/components/Historialcom';
+import Historialcom from '@/components/HistorialCom';
 {/* Pagina de Acceso */}
 
 const Historial = () => {
