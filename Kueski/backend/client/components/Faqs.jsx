4f98ac7f0a84b89af31bc4db7f96b6a8b667f951
StefanoHA <stefano.herrejon@hotmail.com>
2023-05-04 18:08:28 -0600

    FAQ

--- Kueski/backend/client/components/Faqs.jsx
+++ Kueski/backend/client/components/Faqs.jsx
@@ -8,9 +8,7 @@ const Faqs = () => {
                 open
             >
                 <summary className="flex cursor-pointer items-center justify-between gap-1.5">
-                <h2 className="text-lg font-medium text-gray-900">
-                    Con la finalidad de poder ser de utilidad para kueski.
-                </h2>
+                <h2 className="text-lg font-medium text-gray-900">¿Que puedo lograr con esta interfaz?</h2>
 
                 <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
                     <svg
@@ -29,37 +27,8 @@ const Faqs = () => {
                 </summary>
 
                 <p className="mt-4 leading-relaxed text-gray-700">
-                        Proyecto realizado para la facilitación de ejercer de manera correcta los derechos ARCO.
-                </p>
-            </details>
-
-            <details
-                className="group border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden"
-            >
-                <summary className="flex cursor-pointer items-center justify-between gap-1.5">
-                <h2 className="text-lg font-medium text-gray-900">
-                    En la interfaz se cuenta con varias pestañas que se puede navegar entre ellas presionando los botons de la sidebar a la izquierda.
-
-                </h2>
-
-                <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
-                    <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5 shrink-0 transition duration-300 group-open:-rotate-45"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    >
-                    <path
-                        fillRule="evenodd"
-                        d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-                        clipRule="evenodd"
-                    />
-                    </svg>
-                </span>
-                </summary>
-
-                <p className="mt-4 leading-relaxed text-gray-700">
-                    La primera es la Home Page. Es el simbolo del cubo azul. Luego esta la del dashboard, en donde se pueden realizar las peticiones ARCO.
+                A travez de esta interfaz se puede realizar las peticiones ARCO. Se cuenta con distintas paginas, cada una con un funcinoamiento especifico.
+                        La primera es la Home Page,es el simbolo del cubo azul. Luego esta la del dashboard, en donde se pueden realizar las peticiones ARCO.
                      A travaez del botón del reloj se lleva al historial, en donde se puede consultar los registros sobre las distintas peticiones realizadas.
                      Por ultimo esta en FAQ, en donde pondremos información util para poder usar esta herramienta. 
                 </p>
@@ -70,7 +39,8 @@ const Faqs = () => {
             >
                 <summary className="flex cursor-pointer items-center justify-between gap-1.5">
                 <h2 className="text-lg font-medium text-gray-900">
-                    Para dudas o aclaraciones
+                   ACCESO
+
                 </h2>
 
                 <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
@@ -90,7 +60,156 @@ const Faqs = () => {
                 </summary>
 
                 <p className="mt-4 leading-relaxed text-gray-700">
-                    Contacte a los siguientes correos. A0057172@tec.mx
+                    La Acción de acceso permite visualizar los datos almacenador de cualquier cliente. 
+                </p>
+            </details>
+
+            <details
+                className="group border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden"
+            >
+                <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+                <h2 className="text-lg font-medium text-gray-900">
+                    RECTIFICACION
+                </h2>
+
+                <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 shrink-0 transition duration-300 group-open:-rotate-45"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    >
+                    <path
+                        fillRule="evenodd"
+                        d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                        clipRule="evenodd"
+                    />
+                    </svg>
+                </span>
+                </summary>
+
+                <p className="mt-4 leading-relaxed text-gray-700">
+                    La rectificación permite corregir valores de datos almacenados. Solo ingresa los nuevos datos corregidos y presione confirmar.
+                </p>
+            </details>
+
+            <details
+                className="group border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden"
+            >
+                <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+                <h2 className="text-lg font-medium text-gray-900">
+                    CANCELAR
+                </h2>
+
+                <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 shrink-0 transition duration-300 group-open:-rotate-45"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    >
+                    <path
+                        fillRule="evenodd"
+                        d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                        clipRule="evenodd"
+                    />
+                    </svg>
+                </span>
+                </summary>
+
+                <p className="mt-4 leading-relaxed text-gray-700">
+                    Para ejecutar una cancelación hace falta presionar el boton de cancelar de las acciones arco en el usuario a cancelar. 
+                    Una vez dentro de la pagina de cancelar se tendra que oprimir nuevamente un boton, el de "Borrar Datos usuario". 
+                    CUIDADO al presionarlo se borraran  del sistema todos los datos de Identificación.
+                </p>
+            </details>
+
+            <details
+                className="group border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden"
+            >
+                <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+                <h2 className="text-lg font-medium text-gray-900">
+                    OPOSICION
+                </h2>
+
+                <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 shrink-0 transition duration-300 group-open:-rotate-45"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    >
+                    <path
+                        fillRule="evenodd"
+                        d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                        clipRule="evenodd"
+                    />
+                    </svg>
+                </span>
+                </summary>
+
+                <p className="mt-4 leading-relaxed text-gray-700">
+                    En oposicion apareceran toogles que visualmente explican a que consiente el usuario para el uso de sus datos. 
+                    Para cambiarlo hace falta hacerles click.
+                </p>
+            </details>
+
+            <details
+                className="group border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden"
+            >
+                <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+                <h2 className="text-lg font-medium text-gray-900">
+                    HISTORIAL
+                </h2>
+
+                <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 shrink-0 transition duration-300 group-open:-rotate-45"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    >
+                    <path
+                        fillRule="evenodd"
+                        d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                        clipRule="evenodd"
+                    />
+                    </svg>
+                </span>
+                </summary>
+
+                <p className="mt-4 leading-relaxed text-gray-700">
+                    Las acciones arco que se efectuan crean un historial de manera automatica. Para visualiar el historial hace falta ir a la pestaña de historial.
+                    Es a travez del boton que luce como un reloj con una flecha. Dentro de ahi se despliegan el historial de acciones.
+                </p>
+            </details>
+
+            <details
+                className="group border-s-4 border-green-500 bg-gray-50 p-6 [&_summary::-webkit-details-marker]:hidden"
+            >
+                <summary className="flex cursor-pointer items-center justify-between gap-1.5">
+                <h2 className="text-lg font-medium text-gray-900">
+                Interfaz creada por                  </h2>
+
+                <span className="shrink-0 rounded-full bg-white p-1.5 text-gray-900 sm:p-3">
+                    <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-5 w-5 shrink-0 transition duration-300 group-open:-rotate-45"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    >
+                    <path
+                        fillRule="evenodd"
+                        d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+                        clipRule="evenodd"
+                    />
+                    </svg>
+                </span>
+                </summary>
+
+                <p className="mt-4 leading-relaxed text-gray-700">
+                Diego Sebastián García Cabrera, Liliana  Hernández García, Stefano Herrejon Antuñano, Javier Pérez Santiago y Juan Pablo Zambrano Barajas 
+
                 </p>
             </details>
         </div>
